e19c1e84505ca8dcb2620b314aab503d4be5001b
jfbu <2589111+jfbu@users.noreply.github.com>
2021-06-30 14:56:57 +0200

    Finalize build script, add CTAN/ to hold zip of releases

--- texdimens/ctanbuild/private.sh
+++ texdimens/ctanbuild/private.sh
@@ -1,35 +1,39 @@
 #!/bin/sh
-dir="texdimens"
-f="$dir/texdimens.tex"
-g="$f-trimmed"
 aaa="Bu"
+dir="texdimens"
 bubu=" B\."
-ccc="ol"
 
-NLCOMMENT=$(grep -c ^%! $f)
-NLAVANT=$(wc -l < $f)
-# ls -l $f
-echo "$NLCOMMENT sur $NLAVANT lignes à supprimer dans $f."
-sed '/^%!/d' $f > "$g"
-# ls -l $g
-NLAPRES=$(wc -l < "$g")
-echo "$NLAPRES lignes restantes."
-NLDELTA=`expr $NLAVANT - $NLAPRES`
-if [ "$NLCOMMENT" -eq "$NLDELTA" ] ; then
-   cp -f $g $f && rm $g && echo "OK."
-else
-   echo "ERREUR !!"  # will be triggered if last line begins with %! and no EOF
-   exit 1
-fi
+for file in texdimens.tex README.md
+do
+    f="$dir/$file"
+    g="$f-trimmed"
+    NLCOMMENT=$(grep -c ^%! $f)
+    NLAVANT=$(wc -l < $f)
+    # ls -l $f
+    echo "$NLCOMMENT sur $NLAVANT lignes à supprimer dans $f."
+    sed '/^%!/d' $f > "$g"
+    # ls -l $g
+    NLAPRES=$(wc -l < "$g")
+    echo "$NLAPRES lignes restantes."
+    NLDELTA=`expr $NLAVANT - $NLAPRES`
+    if [ "$NLCOMMENT" -eq "$NLDELTA" ] ; then
+	cp -f $g $f && rm $g && echo "OK for $f"
+    else
+	echo "ERREUR !!"  # will be triggered if last line begins with %! and no EOF
+	exit 1
+    fi
+done
 
 bbb="rn"
+ccc="ol"
 jfbu=" ${aaa}${bbb}${ccc}"
 
+echo "Blowing up jfbu cover in LICENSE.md README.md texdimens.tex texdimens.sty ..."
 sed -i "s/$bubu/$jfbu/g" "$dir/LICENSE.md"
 sed -i "s/$bubu/$jfbu/g" "$dir/README.md"
 sed -i "s/$bubu/$jfbu/g" "$dir/texdimens.tex"
 sed -i "s/$bubu/$jfbu/g" "$dir/texdimens.sty"
-echo "Done blowing up jfbu cover in LICENSE.md README.md texdimens.tex texdimens.sty"
+echo "... done"
 
 # Local variables:
 # coding: utf-8
